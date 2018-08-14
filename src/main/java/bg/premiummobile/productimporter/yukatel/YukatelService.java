bc2a70563bb3a561c2e4e57c94837cef2524fd2f
paaavkata <paaavkata@gmail.com>
2018-08-14 09:50:37 +0300

    fully working Yukatel integration.

--- src/main/java/bg/premiummobile/productimporter/yukatel/YukatelService.java
+++ src/main/java/bg/premiummobile/productimporter/yukatel/YukatelService.java
@@ -1,6 +1,10 @@
 package bg.premiummobile.productimporter.yukatel;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,7 +12,6 @@ import java.util.Map;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,18 @@ import bg.premiummobile.productimporter.yukatel.model.YukatelStockInfo;
 
 @Service
 public class YukatelService {
+	
+	public static final char DELIMITER = ',';
+	
+	public static final int EMAG_COMMISSION_PERCENTAGE = 8;
+	
+	public static final int DELIVERY_COMISSION_PERCENTAGE = 1;
+	
+	public static final int DELIVERY_EUROPE_EUR = 5;
+	
+	public static final int DELIVERY_BG_BGN = 5;
+	
+	public static final double EUR_BGN = 1.9559;
 	
 	public List<YukatelStockInfo> getYukatelStockInfo(String sessionId, boolean onlyAvailable) throws IOException{
 		
@@ -35,15 +50,148 @@ public class YukatelService {
 				  .post();
 		Element xjx = doc.selectFirst("xjx");
 		Element cmd = xjx.selectFirst("cmd");
-		Element body = cmd.selectFirst("tbody");
-		Elements rows = body.getElementsByTag("tr");
-		System.out.println(rows.size());
-//		Elements rows = body.getElementsByTag("tr");
-//		for(Element row : rows) {
-//			System.out.println(row.getAllElements().size());
-//		}
-//		String title = doc.title();
-//		System.out.println(title);
-		return null;
+		String cdata = cmd.childNode(0).toString();
+		cdata = cdata.substring(9,cdata.length()-2);
+		Element table = Jsoup.parse(cdata);
+		Element body = table.selectFirst("tbody");
+		Elements rows = body.select("tr");
+		List<YukatelStockInfo> stockList = new ArrayList<>();
+		for(Element row : rows) {
+			Elements columns = row.select("td");
+			if(columns.size() < 5){
+				System.out.println(columns.get(0).data());
+				continue;
+			}
+			String availability = "0";
+			String img = columns.get(2).selectFirst("img").toString();
+			if(img.contains("green")){
+				availability = "1";
+			}
+			String price = columns.get(4).selectFirst("div").selectFirst("input").attr("value").replaceAll(",", ".");
+			stockList.add(new YukatelStockInfo(columns.get(0).childNode(0).toString().trim(), columns.get(1).selectFirst("div").childNode(0).toString().trim(), availability, Double.valueOf(price)));
+		}
+		saveStockInfoProducts(stockList, "yukatel");
+		return stockList;
+	}
+	
+	public void saveStockInfoProducts(List<YukatelStockInfo> products, String name){
+		
+		StringBuilder st = new StringBuilder();
+		st.append("id");
+		st.append(DELIMITER);
+		st.append("name");
+		st.append(DELIMITER);
+		st.append("availability");
+		st.append(DELIMITER);
+		st.append("priceDEwoDel");
+		st.append(DELIMITER);
+		st.append("priceBGwDel");
+		st.append(DELIMITER);
+		st.append("premium");
+		st.append(DELIMITER);
+		st.append("premiumProfit");
+		st.append(DELIMITER);
+		st.append("emag");
+		st.append(DELIMITER);
+		st.append("emagProfit");
+		st.append(System.getProperty("line.separator"));
+		
+		for(YukatelStockInfo product : products){
+			double price = generatePrice(product.getPrice());
+			double magento = generateMagentoPrice(price);
+			double emag = generateEmagPriceWithCommission(price);
+			st.append(product.getId());
+			st.append(DELIMITER);
+			st.append(product.getName());
+			st.append(DELIMITER);
+			st.append(product.getAvailability());
+			st.append(DELIMITER);
+			st.append(product.getPrice());
+			st.append(DELIMITER);
+			st.append(price);
+			st.append(DELIMITER);
+			st.append(magento);
+			st.append(DELIMITER);
+			st.append(magento/1.2 - price);
+			st.append(DELIMITER);
+			st.append(emag);
+			st.append(DELIMITER);
+			st.append(emag/1.2/1.08 - price);
+			st.append(System.getProperty("line.separator"));
+		}
+		
+		writeDataToFile(st.toString(), name);
+		
+	}
+	
+	private int calculateProfit(double price){
+		if(price < 300){
+			return 30;
+		} else if(price < 500){
+			return 40;
+		} else if(price < 750){
+			return 60;
+		} else if(price < 1000){
+			return 70;
+		} else if(price < 1500){
+			return 80;
+		} else if(price < 2000){
+			return 100;
+		} else if(price < 5000){
+			return 150;
+		} else {
+			return 300;
+		}
+	}
+	private double generateMagentoPrice(double price){
+		price+=DELIVERY_BG_BGN;
+		price+=calculateDelivery(price);
+		price+=calculateProfit(price);
+		price*=1.2;
+		return prettyPrice(price);
+	}
+	
+	private double calculateDelivery(double price){
+		//Calculating circa cash on delivery commission.
+		
+		return (price + 10)*1.2*(DELIVERY_COMISSION_PERCENTAGE/100);
+	}
+	
+	private double generatePrice(double price){
+		//Generating price that has to be payed on delivery from EU.
+		return (price + DELIVERY_EUROPE_EUR)*EUR_BGN;
+	}
+	
+	private int prettyPrice(double oldPrice) {
+		int tens = (int) oldPrice/10;
+		int newPrice = tens*10 - 1;
+		return newPrice;
+	}
+
+	private int generateEmagPriceWithCommission(double price){
+		price+=DELIVERY_BG_BGN;
+		price+=calculateDelivery(price);
+		price+=calculateProfit(price);
+		price*=1.08;//(1 + EMAG_COMMISSION_PERCENTAGE/100);
+		price*=1.2;
+		return prettyPrice(price);
+	}
+	
+	private void writeDataToFile(String data, String name){
+		
+		File file = new File(name + ".csv");
+		
+		if(!file.exists() && !file.isDirectory()){
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try (PrintWriter printer = new PrintWriter(file)){
+			printer.println(data);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
 	}
 }
--- src/main/java/bg/premiummobile/productimporter/yukatel/model/YukatelStockInfo.java
+++ src/main/java/bg/premiummobile/productimporter/yukatel/model/YukatelStockInfo.java
@@ -2,4 +2,44 @@ package bg.premiummobile.productimporter.yukatel.model;
 
 public class YukatelStockInfo {
 
+	private String id;
+	private String name;
+	private String availability;
+	private double price;
+	
+	public YukatelStockInfo(){
+		
+	}
+	
+	public YukatelStockInfo(String id, String name, String availability, double price){
+		this.id = id;
+		this.name = name;
+		this.availability = availability;
+		this.price = price;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAvailability() {
+		return availability;
+	}
+	public void setAvailability(String availability) {
+		this.availability = availability;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 }
